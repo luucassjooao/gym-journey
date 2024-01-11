877a65108ec3a0028c509018669f7e3ec1b96592
lucasjoaoo <mainexcalibur@protonmail.com>
2024-01-11 10:10:30 -0300

    :lock: Cors release

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { MuscleExerciseSeed } from 'seed/muscle-exercise.seed';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const seeding = app.get(MuscleExerciseSeed);
   await seeding.verifyIfSeedIsCreated();
